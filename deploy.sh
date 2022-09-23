b54ae8da1bb513b17831baad77f5d0eae7e3e740
Biswajit Nandi <31655168+bjnandi@users.noreply.github.com>
2022-09-23 13:51:03 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # expose the nginx containers
-kubectl --kubeconfig ./kubeconfig expose deployment/my-nginx \
+kubectl expose deployment/my-nginx \
         --port=80 --target-port=80 \
         --name=my-nginx-service --type=LoadBalancer
